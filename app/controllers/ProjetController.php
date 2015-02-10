e3b263c8b79a493dc0e474e5d1bb192ccbe14a9b
N3iluj <Julien.card@gmail.com>
2015-02-10 14:59:43 +0100

    DEV

--- app/controllers/ProjetController.php
+++ app/controllers/ProjetController.php
@@ -31,7 +31,42 @@ class ProjetController extends \BaseController {
 	 */
 	public function store()
 	{
-		//
+		$rules = array('theme' => 'required|max:50|min:3');
+		$validation=Validator::make(Input::all(),$rules);
+  		if ($validation->fails())
+  		{
+  			Session::flash('fail', 'Certain(s) champs sont incorrect');
+   			return Redirect::to('projet/create')->withInput();
+  		}
+  		else
+  		{
+			$projet = new User;
+			$projet-> nom=Input::get('nom');
+			$projet-> prenom=Input::get('prenom');
+			$projet-> date_naissance=Input::get('dateNaissance');
+			$projet-> statut=Input::get('statut');
+			$projet-> adresse=Input::get('adresse');
+			$projet-> cp=Input::get('cp');
+			$projet-> ville=Input::get('ville');
+			$projet-> tel=Input::get('tel');
+			$projet-> email=Input::get('email');
+			$projet-> username=Input::get('pseudo');
+			
+			if(Input::get('password')==Input::get('mdp2')){
+				$projet-> password= Hash::make(Input::get('password'));
+			}
+			else {
+				Session::flash('fail', 'Mots de passe différents');
+				return Redirect::back()->withInput(); 
+			}
+
+			if($projet->save()){
+				return Redirect::to('projet/create');
+			}
+		
+		}
+
+		return Redirect::to('projet/create');
 	}
 
 
--- app/views/projet/create.blade.php
+++ app/views/projet/create.blade.php
@@ -51,7 +51,7 @@
         <div class="form-group">
           {{ Form::label('valeur','Estimez la valeur de votre diaporama (en €)', array('class'=>'col-lg-2 control-label')) }}
           <div class="col-lg-10">
-            {{ Form::text('valeur', null, array('class' => 'form-control', 'placeholder'=>'0')) }}
+            {{ Form::text('valeur', null, array('class' => 'form-control', 'placeholder'=>'0.00')) }}
           </div>
         </div>
         <div class="form-group">
