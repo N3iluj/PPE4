4d60d6c412aa1c4b7609f2b4f91b5f999414440c
DevGoMP <mehdy.poncet@gmail.com>
2015-03-11 10:32:14 +0100

    Dev Admin
    Table + export

--- app/views/user/readall.blade.php
+++ app/views/user/readall.blade.php
@@ -15,7 +15,7 @@
 
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav">
-        <li class="active"><a href="#">Mon profil </a></li>
+        <li class="active">{{HTML::link('user/show', 'Mon profil')}}</li>
          @if (Auth::check())
             @if((Auth::user()->statut=="admin"))
                 <li>{{HTML::link('user/index', 'Administration')}}</li>
@@ -33,23 +33,54 @@
 
 @section('contenu')
 @parent
-
-
+<!-- BOUTON EXPORT -->
+<div class="btn-group open">
+  <a href="#" class="btn btn-primary">Export</a>
+  <a href="#" class="btn btn-primary dropdown-toggle" data-toggle="dropdown" aria-expanded="true"><span class="caret"></span></a>
+  <ul class="dropdown-menu">
+    <li><a href="#">Action</a></li>
+    <li><a href="#">Another action</a></li>
+    <li><a href="#">Something else here</a></li>
+    <li class="divider"></li>
+    <li><a href="#">Separated link</a></li>
+  </ul>
+</div>
 
 
 <!-- TABLEAU -->
+
+<?php $nb = 1 ?>
+
 <table class="table table-striped table-hover ">
   <thead>
     <tr>
       <th>#</th>
-      <th>Column heading</th>
-      <th>Column heading</th>
-      <th>Column heading</th>
+      <th>Pseudo</th>
+      <th>Nom</th>
+      <th>Prénom</th>
+      <th>N° Tel</th>
+      <th>Email</th>
     </tr>
   </thead>
   <tbody>
+@foreach($lesuser as $users)
+      <tr class="info">
+      <td>{{$nb}}</td>
+      <td>{{$users->username}}</td>
+      <td>{{$users->nom}}</td>
+      <td>{{$users->prenom}}</td>
+      <td>{{$users->tel}}</td>
+      <td>{{$users->email}}</td>
+    </tr>
+<?php $nb = $nb+1 ?>
+@endforeach
+
+
+
+  <!--
     <tr>
       <td>1</td>
+      <td>Column content</td>
       <td>Column content</td>
       <td>Column content</td>
       <td>Column content</td>
@@ -59,9 +90,11 @@
       <td>Column content</td>
       <td>Column content</td>
       <td>Column content</td>
+      <td>Column content</td>
     </tr>
     <tr class="info">
       <td>3</td>
+      <td>Column content</td>
       <td>Column content</td>
       <td>Column content</td>
       <td>Column content</td>
@@ -71,9 +104,11 @@
       <td>Column content</td>
       <td>Column content</td>
       <td>Column content</td>
+      <td>Column content</td>
     </tr>
     <tr class="danger">
       <td>5</td>
+      <td>Column content</td>
       <td>Column content</td>
       <td>Column content</td>
       <td>Column content</td>
@@ -83,13 +118,15 @@
       <td>Column content</td>
       <td>Column content</td>
       <td>Column content</td>
+      <td>Column content</td>
     </tr>
     <tr class="active">
       <td>7</td>
       <td>Column content</td>
       <td>Column content</td>
       <td>Column content</td>
-    </tr>
+      <td>Column content</td>
+    </tr>-->
   </tbody>
 </table> 
 
